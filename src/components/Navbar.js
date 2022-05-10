90bcfda3ae727bbae7ee82b201aeef6b4aa85217
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-10 13:50:44 +0300

    Add Navbar component with styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../assets/planet.png';
+import './Navbar.css';
+
+const Navbar = () => (
+  <header className="contained">
+    <div className="logo-title">
+      <img src={logo} alt="Logo" />
+      <h1>Space Travelers Hub</h1>
+    </div>
+  </header>
+);
+
+export default Navbar;
