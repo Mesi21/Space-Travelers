2b96b2a2ff8cbae46ee86472a215ea46ae752ba1
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-10 16:55:02 +0300

    Stylethe status of the mission

--- src/components/MissionStatus.js
+++ src/components/MissionStatus.js
@@ -3,8 +3,18 @@ import PropTypes from 'prop-types';
 
 const MissionStatus = (props) => {
   const { active } = props;
+
+  const statusStyle = {
+    backgroundColor: 'var(--black-1)',
+    borderRadius: '2px',
+    fontSize: '12px',
+    padding: '2px 4px',
+    whiteSpace: 'nowrap',
+    color: 'white',
+  };
+
   return (
-    <h2>
+    <h2 style={statusStyle}>
       { active ? 'Active Member' : 'NOT A MEMBER' }
     </h2>
   );
