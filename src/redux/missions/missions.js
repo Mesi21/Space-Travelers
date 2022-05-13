297263dce7ab6db6d63b8955a732dae2766ae9cf
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-05-13 22:15:02 +0300

    Add Join mission action

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -5,6 +5,7 @@ const url = 'https://api.spacexdata.com/v3/missions';
 const GET_MISSIONS_LOADING = 'spacetravels/GET_MISSIONS_LOADING';
 const GET_MISSIONS_FAILURE = 'spacetravels/GET_MISSIONS_FAILURE';
 const GET_MISSIONS_SUCCESS = 'spacetravels/GET_MISSIONS_SUCCESS';
+const JOIN_MISSION = 'spacetravels/JOIN_MISSIONS';
 
 export const getMissionsLoading = () => ({ type: GET_MISSIONS_LOADING });
 export const getMissionsFailure = (errMessage) => ({
@@ -12,6 +13,7 @@ export const getMissionsFailure = (errMessage) => ({
   payload: errMessage,
 });
 export const getMissionsSuccess = (missions) => ({ type: GET_MISSIONS_SUCCESS, payload: missions });
+export const joinMission = (missionId) => ({ type: JOIN_MISSION, payload: missionId });
 export const getMissions = () => (dispatch) => {
   dispatch(getMissionsLoading());
   fetch(url)
@@ -32,6 +34,16 @@ const missions = (state = initialMissions, action) => {
       return { ...state, loading: true };
     case GET_MISSIONS_SUCCESS:
       return { ...state, missions: action.payload, loading: false };
+    case JOIN_MISSION:
+      return {
+        ...state,
+        missions: state.missions.map((mission) => {
+          if (mission.id !== action.payload) {
+            return mission;
+          }
+          return { ...mission, joined: true };
+        }),
+      };
     default:
       return state;
   }
